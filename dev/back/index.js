6ad6f691ab53b8bfa8d905dacbd6ed5becb4d319
Juan Ronanduano <juanronanduano@MacBook-Pro-de-Juan.local>
2019-03-27 10:30:11 -0300

    Se acomoda metodo para abrir browser y se agrega metodo para terminar el proceso cuando se cierra la ventana

--- dev/back/index.js
+++ dev/back/index.js
@@ -12,10 +12,11 @@ app.get('/', function(request, response){
     response.sendFile(path.join(__dirname+'/../front/index.html'));
 });
 app.get('/mapTree', locationTrees.createTree);
+app.get('/cerrarApp', process.exit);
 
 app.listen(config.port, config.hostname, () => {
     console.log(`Server running at http://${config.hostname}:${config.port}/`);
-    opn(`http://${config.hostname}:${config.port}/`, 'google chrome', function (err) {
+    opn(`http://${config.hostname}:${config.port}/`, {app: ['google chrome']}, function (err) {
       if (err) throw err;
       console.log('The user closed the browser');
       opn(`http://${config.hostname}:${config.port}/`);
