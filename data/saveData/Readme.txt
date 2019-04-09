2cfd1e1b9db50ed6bb0e829689bd4c1f44e795f6
jfronan <juanfronan@hotmail.com>
2019-04-09 00:50:01 -0300

    Se guarda lo viejo en branch

--- data/saveData/Readme.txt
+++
@@ -1,3 +0,0 @@
-Este directorio almacena archivos con datos sobre la ultima vez que se uso la app.
-
-Ej. logs.json tiene la historia completa de mensajes del logger
--- src/Constants.js
+++
@@ -1,9 +0,0 @@
-const remote = window.require('electron').remote;
-const {app} = remote;
-var basepath = app.getAppPath();
-
-export const APPDIR = basepath;
-export const DATADIR = basepath + '/data';
-export const LOCATIONS = basepath + '/data/locations';
-export const PERSONAJES = basepath + '/data/personajes';
-export const SAVEDATA = basepath + '/data/saveData';
