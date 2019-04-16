a4c017a57072388bdc20b2f668f8ddaa2740352e
jfronan <juanfronan@hotmail.com>
2019-04-16 03:15:30 -0300

    Se agrega estilizacion textarea a iframes

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -49,13 +49,22 @@ export const renderIframe = (url, classes)=> {
     var frame = frameRef.target;
     var body = frame.contentWindow.document.body;
     var images = frame.contentWindow.document.images;
+    var textAreas = frame.contentWindow.document.getElementsByTagName('textarea');
     body.style.fontSize = '14px';
-    body.style.whiteSpace = 'pre-line';
     for (var i = 0; i < images.length; i++) {
     images[i].style.maxWidth = '100%';
     images[i].style.maxHeight = '100%';
     images[i].style.objectFit = 'contain';
     images[i].style.alignSelf = 'center';
+    }
+    for (var i = 0; i < textAreas.length; i++) {
+        textAreas[i].style.height = textAreas[i].scrollHeight+"px";
+        textAreas[i].style.width = '100%';
+        textAreas[i].style.border = 'none';
+        textAreas[i].style.padding = '0px';
+        textAreas[i].style.resize = 'none';
+        textAreas[i].style.backgroundColor = 'transparent';
+        textAreas[i].disabled = true;
     }
   };
   return(
